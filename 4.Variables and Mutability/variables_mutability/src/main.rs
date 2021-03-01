e6ebb4dd1bde03c33f751180c1392afb794e0492
dipansharora <dipansharora7@icloud.com>
2021-03-01 11:47:23 -0800

    complete

--- 4.Variables and Mutability/variables_mutability/src/main.rs
+++ 4.Variables and Mutability/variables_mutability/src/main.rs
@@ -1,3 +1,32 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let x = 5;
+    println!("x = {}",x);
+
+    //can't do this, here x is immutable so can't assign a value to x more than once.
+    // x = 10;
+    println!("x = {}",x);
+
+
+    let mut y = 10;
+    println!("y = {}",y);
+
+    y = 20;
+    println!("y = {}",y);
+
+    //shadowing
+    let z = 5;
+    let z = z + 1;
+    let z = z*5;
+
+    println!("z = {}",z);
+
+    let spaces = "    ";
+    let spaces = spaces.len();
+
+    println!("spaces = {}",spaces);
+
+    
+
 }
